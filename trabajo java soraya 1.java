9516d30f9da0907b8fd431b37d407c011d01ad23
maddox2008 <maddoxsbr8@gmail.com>
2025-08-26 20:06:28 -0600

    Add files via upload

---
+++ trabajo java soraya 1.java
@@ -0,0 +1,50 @@
+public class practica1 {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+
+int[] numeros = new int[15];
+int num, contador_neg = 0, contador_positivos = 0, result_promedio = 0, suma_positivos = 0 ;
+
+Scanner captura = new Scanner(System.in);
+
+for(int x = 0; x < numeros.length; x++) {
+System.out.println("Dame el valor: " + x);
+num = captura.nextInt();
+numeros[x] = num;
+}
+
+for(int i = 0; i < numeros.length; i ++) {
+//String n =numeros[i] %2 == 0 ? "hola" : "no";
+
+if(numeros[i] %2 == 0 && numeros[i] > 0) {
+double raiz;
+raiz = Math.sqrt(numeros[i]);
+System.out.println("La raiz cuadrada de " + numeros[i]+ " " +raiz);
+
+}
+
+if(numeros[i] < 0) {
+
+contador_neg += 1;
+contador_neg = contador_neg + 1;
+}
+
+if (numeros[i] %2 !=0) {
+suma_positivos += numeros[i];
+contador_positivos += numeros[i];
+
+
+}
+
+}
+result_promedio = suma_positivos / contador_positivos;
+
+System.out.println("El resultado negaivo es: " + contador_positivos);
+System.out.println("El promedio es: " + result_promedio);
+
+
+}
+
+}
